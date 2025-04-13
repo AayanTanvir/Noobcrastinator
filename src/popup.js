092fd467e06cc0f82a35b801a152ff3e6f5b586e
AayanTanvir <k98778376@gmail.com>
2025-04-13 22:28:10 +0500

    Create blockedSite page and button

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     
+    const blockedSitesURL = chrome.runtime.getURL("blockedSites.html");
+    
+    document.getElementById("openBlockedSites").addEventListener('click', () => {
+        chrome.tabs.create({ url:blockedSitesURL });
+    }) 
+
 })
