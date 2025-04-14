20b1b6298c2410c2ccbe69fb653f949b1d32f0ae
AayanTanvir <k98778376@gmail.com>
2025-04-15 00:03:11 +0500

    added URL input in blocked page

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const blockedSitesURL = chrome.runtime.getURL("blockedSites.html");
-    
-    document.getElementById("openBlockedSites").addEventListener('click', () => {
+    const blockedURLWrapper = document.getElementById("URL-wrapper"); 
+
+    document.getElementById("blockedSitesBtn")?.addEventListener('click', () => {
         chrome.tabs.create({ url:blockedSitesURL });
-    }) 
+    })
+
+    
 
 })
