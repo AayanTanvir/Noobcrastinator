d460fdc16fae07378472c51b8f7e5c611c3d8c8a
AayanTanvir <k98778376@gmail.com>
2025-04-24 12:51:18 +0500

    Setup popup and build configuration

---
+++ src/popup/PopupApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopupApp = () => {
+    return (
+        <div className='w-full h-full'>
+            <h1 className='text-neutral-800'>Hello world</h1>
+        </div>
+    )
+}
+
+export default PopupApp
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +9,14 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                popup: resolve(__dirname, 'popup.html'),
+            },
+        },
+        outDir: "dist",
+    },
+    minify: false,
+    sourcemap: true,
 })
