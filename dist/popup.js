2853ea93ef306576caf2bc4ed71b06a40037fa58
AayanTanvir <k98778376@gmail.com>
2025-04-25 22:01:56 +0500

    v1.0.0

---
+++ dist/popup.js
@@ -0,0 +1,5 @@
+import{j as e,R as r,a as t}from"./assets/client-C-l9ORaP.js";import{B as s}from"./assets/browser-polyfill-CqyFTn3I.js";import"./assets/_commonjsHelpers-CqkleIqs.js";const o=()=>e.jsxs("div",{className:"w-full h-full absolute top-0 left-0 flex flex-col items-center justify-start p-2 gap-2",children:[e.jsxs("h1",{className:"text-3xl cursor-default",children:[e.jsx("span",{className:"text-lime-400",children:"Noob"}),"crastinator"]}),e.jsx("button",{onClick:()=>s.runtime.sendMessage({action:"openBlockedSites"}),className:`w-40 h-12 bg-neutral-600 \r
+            shadow-[0_0_8px_rgba(132,208,0,0.5)] transition duration-150 \r
+            ease-out hover:scale-[103%]\r
+            rounded-md mt-4 mb-2 cursor-pointer border-2\r
+            border-lime-500 hover:border-red-500 hover: `,children:e.jsx("h1",{className:"text-lg font-semibold text-gray-300 text-nowrap",children:"Blocked Sites"})})]});r.createRoot(document.getElementById("root")).render(e.jsx(t.StrictMode,{children:e.jsx(o,{})}));
